6a48c63ea8521a1ac79ff1e20313f7d586340e95
Daniel Jung <56655511+dqj5182@users.noreply.github.com>
2022-02-12 08:49:05 -0500

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+from utils.autoaugment import CIFAR10Policy
+from utils.da import RandomCropPaste
+from model.vit import ViT
+
+
+def get_criterion(args):
+    if args.criterion=="ce":
+        if args.label_smoothing:
+            criterion = LabelSmoothingCrossEntropyLoss(args.num_classes, smoothing=args.smoothing)
+        else:
+            criterion = nn.CrossEntropyLoss()
+    else:
+        raise ValueError(f"{args.criterion}?")
+    return criterion
+
+
+def get_model(args):
+    if args.model_name == 'vit':
+        net = ViT(
+            args.in_c, 
+            args.num_classes, 
+            img_size=args.size, 
+            patch=args.patch, 
+            dropout=args.dropout, 
+            mlp_hidden=args.mlp_hidden,
+            num_layers=args.num_layers,
+            hidden=args.hidden,
+            head=args.head,
+            is_cls_token=args.is_cls_token
+            )
+    else:
+        raise NotImplementedError(f"{args.model_name} is not implemented yet...")
+    return net
+
+
+def get_transform(args):
+    train_transform = []
+    test_transform = []
+    train_transform += [
+        transforms.RandomCrop(size=args.size, padding=args.padding)
+    ]
+    if args.dataset != 'svhn':
+        train_transform += [transforms.RandomHorizontalFlip()]
+    
+    if args.autoaugment:
+        if args.dataset == 'c10' or args.dataset=='c100':
+            train_transform.append(CIFAR10Policy())
+        elif args.dataset == 'svhn':
+            train_transform.append(SVHNPolicy())
+        else:
+            print(f"No AutoAugment for {args.dataset}")   
+
+    train_transform += [
+        transforms.ToTensor(),
+        transforms.Normalize(mean=args.mean, std=args.std)
+    ]
+    if args.rcpaste:
+        train_transform += [RandomCropPaste(size=args.size)]
+    
+    test_transform += [
+        transforms.ToTensor(),
+        transforms.Normalize(mean=args.mean, std=args.std)
+    ]
+
+    train_transform = transforms.Compose(train_transform)
+    test_transform = transforms.Compose(test_transform)
+
+    return train_transform, test_transform
+    
+
+def get_dataset(args):
+    root = "data"
+    if args.dataset == "c10": # CIFAR-10
+        args.in_c = 3
+        args.num_classes=10
+        args.size = 32
+        args.padding = 4
+        args.mean, args.std = [0.4914, 0.4822, 0.4465], [0.2470, 0.2435, 0.2616]
+        train_transform, test_transform = get_transform(args)
+        train_ds = torchvision.datasets.CIFAR10(root, train=True, transform=train_transform, download=True)
+        test_ds = torchvision.datasets.CIFAR10(root, train=False, transform=test_transform, download=True)
+    else:
+        raise NotImplementedError(f"{args.dataset} is not implemented yet.")
+    
+    return train_ds, test_ds
+
+
+def get_experiment_name(args):
+    experiment_name = f"{args.model_name}_{args.dataset}"
+    if args.autoaugment:
+        experiment_name+="_aa"
+    if args.label_smoothing:
+        experiment_name+="_ls"
+    if args.rcpaste:
+        experiment_name+="_rc"
+    if args.cutmix:
+        experiment_name+="_cm"
+    if args.mixup:
+        experiment_name+="_mu"
+    if args.off_cls_token:
+        experiment_name+="_gap"
+    print(f"Experiment:{experiment_name}")
+    return experiment_name
+
+
+class LabelSmoothingCrossEntropyLoss(nn.Module):
+    def __init__(self, classes, smoothing=0.0, dim=-1):
+        super(LabelSmoothingCrossEntropyLoss, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.cls = classes
+        self.dim = dim
+
+    def forward(self, pred, target):
+        pred = pred.log_softmax(dim=self.dim)
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred)
+            true_dist.fill_(self.smoothing / (self.cls - 1))
+            true_dist.scatter_(1, target.data.unsqueeze(1), self.confidence)
+        return torch.mean(torch.sum(-true_dist * pred, dim=self.dim))    
