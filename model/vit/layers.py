44b78b612b8729e48630ca14e11c5a5c4bdcaf63
dqj5182 <dqj5182@psu.edu>
2022-02-17 21:42:49 +0900

    commit

--- model/vit/layers.py
+++ model/vit/layers.py
@@ -1,49 +1,48 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchsummary
 
 
 class TransformerEncoder(nn.Module):
-    def __init__(self, feats:int, mlp_hidden:int, head:int=8, dropout:float=0.):
+    def __init__(self, features:int, mlp_hidden:int, head:int=8, dropout:float=0.):
         super(TransformerEncoder, self).__init__()
-        self.la1 = nn.LayerNorm(feats)
-        self.msa = MultiHeadSelfAttention(feats, head=head, dropout=dropout)
-        self.la2 = nn.LayerNorm(feats)
+        self.norm1 = nn.LayerNorm(features)
+        self.msa = MultiHeadAttention(features, head=head, dropout=dropout)
+        self.norm2 = nn.LayerNorm(features)
         self.mlp = nn.Sequential(
-            nn.Linear(feats, mlp_hidden),
+            nn.Linear(features, mlp_hidden),
             nn.GELU(),
             nn.Dropout(dropout),
-            nn.Linear(mlp_hidden, feats),
+            nn.Linear(mlp_hidden, features),
             nn.GELU(),
             nn.Dropout(dropout),
         )
 
-    def forward(self, x):
-        out = self.msa(self.la1(x)) + x
-        out = self.mlp(self.la2(out)) + out
+    def forward(self, input):
+        out = self.msa(self.norm1(input)) + input
+        out = self.mlp(self.norm2(out)) + out
         return out
 
 
-class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, feats:int, head:int=8, dropout:float=0.):
-        super(MultiHeadSelfAttention, self).__init__()
+class MultiHeadAttention(nn.Module):
+    def __init__(self, features:int, head:int=8, dropout:float=0.):
+        super(MultiHeadAttention, self).__init__()
         self.head = head
-        self.feats = feats
-        self.sqrt_d = self.feats**0.5
+        self.features = features
+        self.sqrt_d = self.features**0.5
 
-        self.q = nn.Linear(feats, feats)
-        self.k = nn.Linear(feats, feats)
-        self.v = nn.Linear(feats, feats)
+        self.q = nn.Linear(features, features)
+        self.k = nn.Linear(features, features)
+        self.v = nn.Linear(features, features)
 
-        self.o = nn.Linear(feats, feats)
+        self.o = nn.Linear(features, features)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x):
-        b, n, f = x.size()
-        q = self.q(x).view(b, n, self.head, self.feats//self.head).transpose(1,2)
-        k = self.k(x).view(b, n, self.head, self.feats//self.head).transpose(1,2)
-        v = self.v(x).view(b, n, self.head, self.feats//self.head).transpose(1,2)
+    def forward(self, input):
+        b, n, f = input.size()
+        q = self.q(input).view(b, n, self.head, self.features//self.head).transpose(1,2)
+        k = self.k(input).view(b, n, self.head, self.features//self.head).transpose(1,2)
+        v = self.v(input).view(b, n, self.head, self.features//self.head).transpose(1,2)
 
         score = F.softmax(torch.einsum("bhif, bhjf->bhij", q, k)/self.sqrt_d, dim=-1) #(b,h,n,n)
         attn = torch.einsum("bhij, bhjf->bihf", score, v) #(b,n,h,f//h)
