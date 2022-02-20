eab985ddb5cdf6a44633798846947f591127e93b
Daniel Jung <56655511+dqj5182@users.noreply.github.com>
2022-02-20 15:52:18 +0900

    Update layers.py

--- model/vit/layers.py
+++ model/vit/layers.py
@@ -36,7 +36,7 @@ class TransformerEncoder(nn.Module): # Done
         # input_dim and head for Multi-Head Attention
         super(TransformerEncoder, self).__init__()
         self.norm1 = nn.LayerNorm(input_dim) # LayerNorm is BatchNorm for NLP
-        self.msa = MultiHeadSelfAttention(input_dim, num_head=num_head, dropout=dropout)
+        self.msa = MultiHeadSelfAttention(input_dim, n_heads=num_head)
         self.norm2 = nn.LayerNorm(input_dim)
         # Position-wise Feed-Forward Networks with GELU activation functions
         self.mlp = nn.Sequential(
@@ -52,6 +52,101 @@ class TransformerEncoder(nn.Module): # Done
         return out
 
 
+class MultiHeadSelfAttention(nn.Module):
+    """Attention mechanism.
+
+    Parameters
+    ----------
+    dim: int
+        The input and out dimension of per token features
+
+    n_heads: int
+        Number of attention heads
+    
+    qkv_bias: bool
+        If True then we include bias to the query, key, and value projections
+
+    attn_p: float
+        Dropout probability applied to the query, key, and value tensors
+
+    proj_p: float
+        Droupout probability applied to the output tensor
+
+    Attributes
+    ----------
+    scale: float
+        Normalizing constant for the dot product
+    
+    qkv: nn.Linear
+        Linear projection for the query, key, and value
+    
+    proj: nn.Linear
+        Linear mapping that takes in the concatenated output of all attention
+        heads and maps it into a new space
+
+    attn_drop, proj_drop: nn.Linear
+        Dropout Layers
+    """
+    def __init__(self, dim:int, n_heads:int=8, qkv_bias:bool=True, attn_p:float=0., proj_p:float=0.):
+        super(MultiHeadSelfAttention, self).__init__()
+        self.n_heads = n_heads
+        self.dim = dim
+        self.head_dim = dim // n_heads # Preparing for concat
+        self.scale = self.head_dim ** -0.5
+
+        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_p)
+        self.proj = nn.Linear(dim, dim)
+        self.proj_drop = nn.Dropout(proj_p)
+    
+    def forward(self, x):
+        """Run forward pass
+
+        Parameters
+        ----------
+        x: torch.Tensor
+            Shape '(n_samples, n_patches + 1, dim)'
+
+        Returns
+        -------
+        torch.Tensor
+            Shape '(n_samples, n_patches + 1, dim)'
+        """
+        n_samples, n_tokens, dim = x.shape
+
+        if dim != self.dim: # To make sure concatnated dim (output dim) is same as input dim
+            raise ValueError
+        
+        # Linear Layers for Query, Key, Value
+        qkv = self.qkv(x) # (n_samples, n_patches+1, 3*dim)
+        qkv = qkv.reshape(n_samples, n_tokens, 3, self.n_heads, self.head_dim) # (n_samples, n_patches+1, 3, n_heads, head_dim)
+        qkv = qkv.permute(2, 0, 3, 1, 4) # (3, n_samples, n_heads, n_patches+1, head_dim)
+        q, k, v = qkv[0], qkv[1], qkv[2] # (n_samples, n_heads, n_patches+1, head_dim)
+
+        # Scaled Dot-Product Attention
+        k_t = k.transpose(-2, -1) # K Transpose: (n_samples, n_heads, head_dim, n_patches+1)
+        dot_product = (q @ k_t)*self.scale # Query, Key Dot Product with Scale Factor: (n_samples, n_heads, n_patches+1, n_patches+1)
+        attn = dot_product.softmax(dim=-1) # Softmax: (n_samples, n_heads, n_patches+1, n_patches+1)
+        attn = self.attn_drop(attn) # Attention Dropout: (n_samples, n_heads, n_patches+1, n_patches+1)
+        weighted_avg = attn @ v # (n_samples, n_heads, n_patches+1, head_dim)
+        weighted_avg = weighted_avg.transpose(1, 2) # (n_samples, n_patches+1, n_heads, head_dim)
+
+        # Concat and Last Linear Layer
+        weighted_avg = weighted_avg.flatten(2) # Concat: (n_samples, n_patches+1, dim)
+        x = self.proj(weighted_avg) # Last Linear Layer: (n_samples, n_patches+1, dim)
+        x = self.proj_drop(x) # Last Linear Layer Dropout: (n_samples, n_patches+1, dim)
+
+        return x
+
+
+
+
+
+
+
+
+
+"""
 class MultiHeadSelfAttention(nn.Module):
     def __init__(self, input_dim:int, num_head:int=8, dropout:float=0.):
         super(MultiHeadSelfAttention, self).__init__()
@@ -85,3 +180,4 @@ class MultiHeadSelfAttention(nn.Module):
         # Concat and Linear
         out = self.dropout(self.lll(attn.flatten(2)))
         return out
+"""
