4a267d8c4bb535f8c12f95ab670aa177a0c77cf3
dqj5182 <dqj5182@psu.edu>
2022-02-20 16:27:21 +0900

    commit

--- model/vit/layers.py
+++ model/vit/layers.py
@@ -4,13 +4,13 @@ import torch.nn.functional as F
 
 
 class PatchEmbedding(nn.Module): # Done
+    """
+    img_size: 1d size of each image (32 for CIFAR-10)
+    patch_size: 1d size of each patch (img_size/num_patch_1d, 4 in this experiment)
+    in_chans: input channel (3 for RGB images)
+    emb_dim: flattened length for each token (or patch)
+    """
     def __init__(self, img_size:int, patch_size:int, in_chans:int=3, emb_dim:int=48):
-        """
-        img_size: 1d size of each image (32 for CIFAR-10)
-        patch_size: 1d size of each patch (img_size/num_patch_1d, 4 in this experiment)
-        in_chans: input channel (3 for RGB images)
-        emb_dim: flattened length for each token (or patch)
-        """
         super(PatchEmbedding, self).__init__()
         self.img_size = img_size
         self.patch_size = patch_size
@@ -46,138 +46,62 @@ class TransformerEncoder(nn.Module): # Done
             nn.GELU(),
         )
 
-    def forward(self, input):
-        out = self.msa(self.norm1(input)) + input # add residual connection
+    def forward(self, x):
+        out = self.msa(self.norm1(x)) + x # add residual connection
         out = self.mlp(self.norm2(out)) + out # add another residual connection
         return out
 
 
 class MultiHeadSelfAttention(nn.Module):
-    """Attention mechanism.
-
-    Parameters
-    ----------
-    dim: int
-        The input and out dimension of per token features
-
-    n_heads: int
-        Number of attention heads
-    
-    qkv_bias: bool
-        If True then we include bias to the query, key, and value projections
-
-    attn_p: float
-        Dropout probability applied to the query, key, and value tensors
-
-    proj_p: float
-        Droupout probability applied to the output tensor
-
-    Attributes
-    ----------
-    scale: float
-        Normalizing constant for the dot product
-    
-    qkv: nn.Linear
-        Linear projection for the query, key, and value
-    
-    proj: nn.Linear
-        Linear mapping that takes in the concatenated output of all attention
-        heads and maps it into a new space
-
-    attn_drop, proj_drop: nn.Linear
-        Dropout Layers
     """
-    def __init__(self, dim:int, n_heads:int=8, qkv_bias:bool=True, attn_p:float=0., proj_p:float=0.):
+    dim: dimension of input and out per token features (emb dim for tokens)
+    n_heads: number of heads
+    qkv_bias: whether to have bias in qkv linear layers
+    attn_p: dropout probability for attention
+    proj_p: droupout probability last linear layer
+    scale: scaling factor for attention (1/sqrt(dk))
+    qkv: initial linear layer for the query, key, and value
+    proj: last linear layer
+    attn_drop, proj_drop: dropout layers for attn and proj
+    """
+    def __init__(self, dim:int, n_heads:int=8, qkv_bias:bool=True, attn_p:float=0.01, proj_p:float=0.01):
         super(MultiHeadSelfAttention, self).__init__()
         self.n_heads = n_heads
-        self.dim = dim
-        self.head_dim = dim // n_heads # Preparing for concat
-        self.scale = self.head_dim ** -0.5
+        self.dim = dim # embedding dimension for input
+        self.head_dim = dim // n_heads # d_q, d_k, d_v in the paper (int div needed to preserve input dim = output dim)
+        self.scale = self.head_dim ** -0.5 # 1/sqrt(d_k)
 
-        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
+        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias) # lower linear layers in Figure 2 of the paper
         self.attn_drop = nn.Dropout(attn_p)
-        self.proj = nn.Linear(dim, dim)
+        self.proj = nn.Linear(dim, dim) # upper linear layers in Figure 2 of the paper
         self.proj_drop = nn.Dropout(proj_p)
     
     def forward(self, x):
-        """Run forward pass
-
-        Parameters
-        ----------
-        x: torch.Tensor
-            Shape '(n_samples, n_patches + 1, dim)'
-
-        Returns
-        -------
-        torch.Tensor
-            Shape '(n_samples, n_patches + 1, dim)'
         """
-        n_samples, n_tokens, dim = x.shape
+        Input and Output shape: [batch_size, n_patches + 1, dim]
+        """
+        batch_size, n_tokens, dim = x.shape # n_tokens = n_patches + 1 (1 is cls_token)
 
-        if dim != self.dim: # To make sure concatnated dim (output dim) is same as input dim
+        if dim != self.dim: # make sure concatnated dim (output dim) is same as input dim
             raise ValueError
         
         # Linear Layers for Query, Key, Value
-        qkv = self.qkv(x) # (n_samples, n_patches+1, 3*dim)
-        qkv = qkv.reshape(n_samples, n_tokens, 3, self.n_heads, self.head_dim) # (n_samples, n_patches+1, 3, n_heads, head_dim)
-        qkv = qkv.permute(2, 0, 3, 1, 4) # (3, n_samples, n_heads, n_patches+1, head_dim)
-        q, k, v = qkv[0], qkv[1], qkv[2] # (n_samples, n_heads, n_patches+1, head_dim)
+        qkv = self.qkv(x) # (batch_size, n_patches+1, 3*dim)
+        qkv = qkv.reshape(batch_size, n_tokens, 3, self.n_heads, self.head_dim) # (batch_size, n_patches+1, 3, n_heads, head_dim)
+        qkv = qkv.permute(2, 0, 3, 1, 4) # (3, batch_size, n_heads, n_patches+1, head_dim)
+        q, k, v = qkv[0], qkv[1], qkv[2] # (batch_size, n_heads, n_patches+1, head_dim)
 
         # Scaled Dot-Product Attention
-        k_t = k.transpose(-2, -1) # K Transpose: (n_samples, n_heads, head_dim, n_patches+1)
-        dot_product = (q @ k_t)*self.scale # Query, Key Dot Product with Scale Factor: (n_samples, n_heads, n_patches+1, n_patches+1)
-        attn = dot_product.softmax(dim=-1) # Softmax: (n_samples, n_heads, n_patches+1, n_patches+1)
-        attn = self.attn_drop(attn) # Attention Dropout: (n_samples, n_heads, n_patches+1, n_patches+1)
-        weighted_avg = attn @ v # (n_samples, n_heads, n_patches+1, head_dim)
-        weighted_avg = weighted_avg.transpose(1, 2) # (n_samples, n_patches+1, n_heads, head_dim)
+        k_t = k.transpose(-2, -1) # K Transpose: (batch_size, n_heads, head_dim, n_patches+1)
+        dot_product = (q @ k_t)*self.scale # Query, Key Dot Product with Scale Factor: (batch_size, n_heads, n_patches+1, n_patches+1)
+        attn = dot_product.softmax(dim=-1) # Softmax: (batch_size, n_heads, n_patches+1, n_patches+1)
+        attn = self.attn_drop(attn) # Attention Dropout: (batch_size, n_heads, n_patches+1, n_patches+1)
+        weighted_avg = attn @ v # (batch_size, n_heads, n_patches+1, head_dim)
+        weighted_avg = weighted_avg.transpose(1, 2) # (batch_size, n_patches+1, n_heads, head_dim)
 
         # Concat and Last Linear Layer
-        weighted_avg = weighted_avg.flatten(2) # Concat: (n_samples, n_patches+1, dim)
-        x = self.proj(weighted_avg) # Last Linear Layer: (n_samples, n_patches+1, dim)
-        x = self.proj_drop(x) # Last Linear Layer Dropout: (n_samples, n_patches+1, dim)
+        weighted_avg = weighted_avg.flatten(2) # Concat: (batch_size, n_patches+1, dim)
+        x = self.proj(weighted_avg) # Last Linear Layer: (batch_size, n_patches+1, dim)
+        x = self.proj_drop(x) # Last Linear Layer Dropout: (batch_size, n_patches+1, dim)
 
         return x
-
-
-
-
-
-
-
-
-
-"""
-class MultiHeadSelfAttention(nn.Module):
-    def __init__(self, input_dim:int, num_head:int=8, dropout:float=0.):
-        super(MultiHeadSelfAttention, self).__init__()
-        self.num_head = num_head
-        self.input_dim = input_dim
-        self.sqrt_dim = self.input_dim**0.5 # sqrt(d_k): scaling factor to prevent gradient vanishing
-
-        # Linear Layer for Value
-        self.ll_value = nn.Linear(input_dim, input_dim)
-        # Linear Layer for Key
-        self.ll_key = nn.Linear(input_dim, input_dim)
-        # Linear Layer for Query
-        self.ll_query = nn.Linear(input_dim, input_dim)
-
-        # Last Linear Layer
-        self.lll = nn.Linear(input_dim, input_dim)
-        self.dropout = nn.Dropout(dropout) # Dropout for Last Linear Layer
-
-    def forward(self, input):
-        batch, num_token, emb_dim = input.size() # batch: batch, num_token: number of words (or patches), emb_dim: embedding (feature) dimensions, [b, n, f]
-        
-        # Linear Layer for Value, Key, Query
-        query = self.ll_query(input).view(batch, num_token, self.num_head, self.input_dim//self.num_head).transpose(1,2) # [batch, self.num_head, num_token, self.input_dim//self.num_head]
-        key = self.ll_key(input).view(batch, num_token, self.num_head, self.input_dim//self.num_head).transpose(1,2) # [batch, self.num_head, num_token, self.input_dim//self.num_head]
-        value = self.ll_value(input).view(batch, num_token, self.num_head, self.input_dim//self.num_head).transpose(1,2) # [batch, self.num_head, num_token, self.input_dim//self.num_head]
-
-        # Scaled Dot-Product Attention
-        attn_score = F.softmax(torch.einsum("bhtf, bhjf->bhtj", query, key)/self.sqrt_dim, dim=-1) #(b,h,n,n)
-        attn = torch.einsum("bhij, bhjf->bihf", attn_score, value) #(b,n,h,f//h)
-
-        # Concat and Linear
-        out = self.dropout(self.lll(attn.flatten(2)))
-        return out
-"""
